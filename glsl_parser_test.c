3f90a43eeaab309df2faff02566a7dd3751da76f
Niels Nesse <nnesse@sonic.net>
2015-09-18 23:04:05 -0700

    Forgot to add glsl_parser_test.c

---
+++ glsl_parser_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "glsl_parser.h"
+#include "glsl_ast.h"
+
+int main()
+{
+	struct glsl_parse_context context;
+
+	glsl_parse_context_init(&context);
+
+	glsl_parse(&context);
+
+	if (context.root) {
+		printf("\nAST tree:\n\n");
+		glsl_print_ast_tree(context.root, 0);
+
+		printf("\nRegenerated GLSL:\n\n");
+		glsl_regen_tree(context.root, 0);
+	}
+
+	glsl_parse_context_destroy(&context);
+}
