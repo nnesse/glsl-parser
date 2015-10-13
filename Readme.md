c7960ede7983c11b91375a1ac8bd8840a1dd3339
Niels Nesse <nnesse@sonic.net>
2015-10-12 19:16:07 -0700

    Support file parameters in parser test program

--- Readme.md
+++ Readme.md
@@ -5,4 +5,4 @@ This is a GLSL parser implemented with `flex` and `bison`. The grammar is based 
 
 The header file `glsl_parser.h` documents the interface to the parser and the header file `glsl_ast.h` defines some useful functions for working with the AST it generates.
 
-The included Makefile builds a test program that reads a shader from standard input and prints out a human readable version of the shader's AST and attempts to regenerate the input shader from the AST.
+The included Makefile builds a test program that reads a shader from a file or standard input and prints out a human readable version of the shader's AST and attempts to regenerate the input shader from the AST.
--- glsl_parser_test.c
+++ glsl_parser_test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "glsl_parser.h"
 #include "glsl_ast.h"
 
@@ -8,7 +10,22 @@ void error_cb(const char *str, int lineno, int first_col, int last_col)
 	fprintf(stderr, "GLSL parse error line %d(%d-%d): %s\n", lineno, first_col, last_col, str);
 }
 
-int main()
+void parse_file(struct glsl_parse_context *context, FILE *f)
+{
+	bool error = glsl_parse_file(context, f);
+
+	if (!error && context->root) {
+		printf("\nAST tree:\n\n");
+		glsl_ast_print(context->root, 0);
+
+		printf("\nRegenerated GLSL:\n\n");
+		char *out = glsl_ast_generate_glsl(context->root);
+		printf("%s", out);
+		free(out);
+	}
+}
+
+int main(int argc, char **argv, char **envp)
 {
 	struct glsl_parse_context context;
 
@@ -16,16 +33,21 @@ int main()
 
 	glsl_parse_set_error_cb(&context, error_cb);
 
-	bool error = glsl_parse_file(&context, stdin);
-
-	if (!error && context.root) {
-		printf("\nAST tree:\n\n");
-		glsl_ast_print(context.root, 0);
-
-		printf("\nRegenerated GLSL:\n\n");
-		char *out = glsl_ast_generate_glsl(context.root);
-		printf("%s", out);
-		free(out);
+	if (argc == 1) {
+		parse_file(&context, stdin);
+	}
+	else {
+		int i;
+		for (i = 1; i < argc; i++) {
+			FILE *f = fopen(argv[i], "rt");
+			if (!f) {
+				fprintf(stderr, "Couldn't open file %s: %s\n", argv[i], strerror(errno));
+				continue;
+			}
+			printf("Input file: %s\n", argv[i]);
+			parse_file(&context, f);
+			fclose(f);
+		}
 	}
 
 	glsl_parse_context_destroy(&context);
